4aeaae63605c229d66b396516d1dee4002320206
Cheezy <cmeier@lifetimefitness.com>
2017-11-06 14:53:48 -0700

    Adding examples

--- PasswordPermutations.scala
+++ PasswordPermutations.scala
@@ -17,6 +17,17 @@ object Test {
             }
           }
       }
-    }.foreach(println)
+    }.foreach(e => {
+      println(e)
+    })
   }
 }
+
+Test.run(List("hello", "world", "asvardsa"), Map('a' -> "@", 'e' -> "3", 'l' -> "1", 'o' -> "0", 'h' -> "|-|"))
+/*
+ * List(hello, |-|ello, h3llo, |-|3llo, he1lo, |-|e1lo, h31lo, |-|31lo, hel1o, |-|el1o, h3l1o, |-|3l1o, he11o, |-|e11o, h311o, |-|311o, hell0, |-|ell0, h3ll0, |-|3ll0, he1l0, |-|e1l0, h31l0, |-|31l0, hel10, |-|el10, h3l10, |-|3l10, he110, |-|e110, h3110, |-|3110)
+ * List(world, w0rld, wor1d, w0r1d)
+ * List(asvardsa, @svardsa, asv@rdsa, @sv@rdsa, asvards@, @svards@, asv@rds@, @sv@rds@)
+ *
+ */
+
